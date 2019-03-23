2995640e9aafe9bd53ec91c343dbeaf354ce290a
ryrocks <ract1436@gmail.com>
2019-03-23 21:30:41 +1100

    Set up enviroment

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+<footer></footer>
+</template>
+<script lang="js">
+export default {
+    name: 'Footer'
+}
+</script>
+
+<style lang="less" scoped>
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <header></header>
+</template>
+<script lang="js">
+export default {
+    name: 'Header'
+}
+</script>
+
+<style lang="less" scoped>
+</style>
